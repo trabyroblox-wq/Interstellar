0659ec1990ad883f4d4647d24a87c401caaf6f2a
trabyroblox-wq <trabyroblox@gmail.com>
2026-02-06 17:23:39 -0500

    Create curs.js

---
+++ curs.js
@@ -0,0 +1,8 @@
+<script>
+  const cursor = document.getElementById("custom-cursor");
+
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+  });
+</script>
